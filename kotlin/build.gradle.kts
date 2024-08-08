d71b07c223a77ccd03b7bd2b07013c2698c2a467
Paul Whalley <81192046+whalleyph@users.noreply.github.com>
2024-08-08 14:14:58 +0100

    Initial commit

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    application
+}
+
+group = "org.sammancoaching"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
