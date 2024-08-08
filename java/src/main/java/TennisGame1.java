3d198405c74435964b445abc34cf29656120b202
whalleyph <walypa56@gmail.com>
2024-08-08 15:58:16 +0100

    game1 code quality update

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,11 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +13,58 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (Objects.equals(playerName, player1Name)) {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+        StringBuilder score = new StringBuilder();
+        int tempScore;
+        if (player1Score == player2Score) {
+            score = switch (player1Score) {
+                case 0 -> new StringBuilder("Love-All");
+                case 1 -> new StringBuilder("Fifteen-All");
+                case 2 -> new StringBuilder("Thirty-All");
+                default -> new StringBuilder("Deuce");
+            };
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) {
+                score = new StringBuilder("Advantage " + player1Name);
+            } else if (minusResult == -1) {
+                score = new StringBuilder("Advantage " + player2Name);
+            } else if (minusResult >= 2) {
+                score = new StringBuilder("Win for " + player1Name);
+            } else {
+                score = new StringBuilder("Win for " + player2Name);
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    tempScore = player1Score;
+                } else {
+                    score.append("-");
+                    tempScore = player2Score;
+                }
+                switch(tempScore) {
                     case 0:
-                        score+="Love";
+                        score.append("Love");
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score.append("Fifteen");
                         break;
                     case 2:
-                        score+="Thirty";
+                        score.append("Thirty");
                         break;
                     case 3:
-                        score+="Forty";
+                        score.append("Forty");
                         break;
                 }
             }
         }
-        return score;
+        return score.toString();
     }
 }
